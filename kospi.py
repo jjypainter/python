8576d4fae6c5900eb6baf36f9dc1b9a781dad2c4
jjypainter <jypainter0612@gmail.com>
2019-12-19 14:20:06 +0900

    string은 introduce로 바꿈. 코스피 추가설치및 코드 추가

--- kospi.py
+++ kospi.py
@@ -1,3 +1,16 @@
 #네이버 증시 페이지에 대신 접속해서,
 #현재 코스피 지수를 가져오는 프로그램을 짤꺼에염.
 
+import requests
+import bs4
+
+#이 주소로 요청을 보내면, 응답으로 html파일이 도착할것.
+html=requests.get('https://finance.naver.com/sise/sise_index.nhn?code=KOSPI')
+#html text를 내가 보기좋게 접근할 수 있도록 변경.
+soup=bs4.BeautifulSoup(html.text,'html.parser')
+#css selector로 내가 원하는 태그를 가져오겠다
+kospi=soup.select_one('#now_value')
+
+print(kospi.text)
+
+# #now_value
